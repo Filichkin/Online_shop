791f1a652454a0b7bd55e0efb52d1de4858f014b
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-18 22:57:18 +0300

    add class Coupon

--- myshop/coupons/models.py
+++ myshop/coupons/models.py
@@ -1,3 +1,33 @@
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator
+)
 from django.db import models
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(
+        max_length=50,
+        unique=True,
+        verbose_name='Код купона'
+    )
+    valid_from = models.DateTimeField(
+        verbose_name='Начало действия'
+    )
+    valid_to = models.DateTimeField(
+        verbose_name='Окончание действия'
+    )
+    discount = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(100)
+            ],
+        help_text='Уровень в процентах от 0 до 100',
+        verbose_name='Скидка'
+    )
+    active = models.BooleanField(
+        verbose_name='Статус активации'
+    )
+
+    def __str__(self):
+        return self.code
