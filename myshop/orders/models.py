3e8c78ade6cde2bdfb07fcc03892cbc778c0660f
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-22 23:03:45 +0300

    update get_total_cost for orders model

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -78,7 +78,8 @@ class Order(models.Model):
         return f'Заказ {self.id}'
 
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()
 
     def get_stripe_url(self):
         if not self.stripe_id:
@@ -88,6 +89,15 @@ class Order(models.Model):
         else:
             path = '/'
         return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
 
 class OrderItem(models.Model):
